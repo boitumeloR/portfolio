a073442f8b430fe000642949735e614c8fcdffde
Boitumelo Rampete <u17120323@tuks.co.za>
2020-09-22 01:41:26 +0200

    Video slot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import {DOCUMENT} from '@angular/common';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
@@ -11,9 +16,11 @@ export class AppComponent implements OnInit {
   loggedIn = false;
   socialLogin = false;
   itineraryCount = 0;
-  constructor(private router: Router) { }
+  constructor(private router: Router, @Inject(DOCUMENT) private document: Document) { }
 
+  @ViewChild('menu', { static: true }) menu: ElementRef<HTMLDivElement>;
   ngOnInit(): void {
+    this.initAnimations();
   }
 
   toggleBurger() {
@@ -22,6 +29,31 @@ export class AppComponent implements OnInit {
   Scroll($element: HTMLElement) {
     console.log($element);
     $element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+  }
+
+  initAnimations() {
+    gsap.from(document.querySelector('.text'), {
+      duration: 2.2,
+      opacity: 0,
+      y: -60,
+      delay: 0.5,
+    });
+
+    gsap.from(this.menu.nativeElement , {
+      duration: 2,
+      opacity: 0,
+      y: -20,
+      delay: 0.5,
+      stagger: 0.3
+    });
+
+    gsap.from(document.querySelector('.back') , {
+      duration: 2,
+      opacity: 0,
+      x: -30,
+      delay: 0.5,
+      stagger: 0.3
+    });
   }
 
 }
