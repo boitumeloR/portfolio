48abdf1f52dfe233953b01125d63bf379c2ee570
Boitumelo Rampete <u17120323@tuks.co.za>
2020-09-21 23:31:28 +0200

    Landing page, no animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit {
+  opened = false;
+  loggedIn = false;
+  socialLogin = false;
+  itineraryCount = 0;
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  toggleBurger() {
+    this.opened = !this.opened;
+  }
+  Scroll($element: HTMLElement) {
+    console.log($element);
+    $element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+  }
+
 }
