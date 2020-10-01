46cd355940eed97f1019524f1dee8f6870d5e276
Boitumelo Rampete <u17120323@tuks.co.za>
2020-10-01 19:22:21 +0200

    Last

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { from } from 'rxjs';
+import { AppComponent } from './app.component';
+import { CViewComponent } from './cview/cview.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'CV',
+    component: CViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,87 +11,14 @@ gsap.registerPlugin(ScrollTrigger);
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
   opened = false;
   loggedIn = false;
   socialLogin = false;
   itineraryCount = 0;
-  constructor(private router: Router, @Inject(DOCUMENT) private document: Document) { }
-
-  @ViewChild('menu', { static: true }) menu: ElementRef<HTMLDivElement>;
   ngOnInit(): void {
-    this.initAnimations();
-    this.observeNav();
+
   }
 
-  ngAfterViewInit(): void {
-    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    // Add 'implements AfterViewInit' to the class.
-    this.observeNav();
-  }
-
-  observeNav() {
-    const header = document.querySelector('.sticky');
-    const intro = document.querySelector('.image');
-
-    const sectionOneObserver = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
-      entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-          console.log('Im intersecting');
-          header.classList.add('nav-white');
-        } else {
-          console.log('im not');
-          header.classList.remove('nav-white');
-        }
-      });
-    }, { rootMargin: '-100px 0px 0px 0px'});
-
-    sectionOneObserver.observe(intro);
-  }
-
-  toggleBurger() {
-    this.opened = !this.opened;
-  }
-  Scroll($element: HTMLElement) {
-    console.log($element);
-    $element.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
-  }
-
-  scrollAnimations() {
-    gsap.to(this.document.querySelector('.video'), {
-      scrollTrigger: {
-        trigger: this.document.querySelector('.video'),
-        start: 'bottom top',
-      } as gsap.plugins.ScrollTriggerInstanceVars,
-      color: '#fff',
-      duration: 2.5,
-      y: 75
-    });
-  }
-
-  initAnimations() {
-    gsap.from(document.querySelector('.text'), {
-      duration: 2.2,
-      opacity: 0,
-      y: -60,
-      delay: 0.5,
-    });
-
-    gsap.from(this.menu.nativeElement , {
-      duration: 2,
-      opacity: 0,
-      y: -20,
-      delay: 0.5,
-      stagger: 0.3
-    });
-
-    gsap.from(document.querySelector('.back') , {
-      duration: 2,
-      opacity: 0,
-      x: -30,
-      delay: 0.5,
-      stagger: 0.3
-    });
-  }
 
 }
